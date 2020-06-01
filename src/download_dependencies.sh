66ed3255da6d90433478f9d945a22ab0f60bdc2f
root <ivi3001@yahoo.de>
2020-06-01 19:44:45 +0000

    added dependencies download script

---
+++ src/download_dependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# clone gmapping package
+# http://wiki.ros.org/gmapping
+git clone https://github.com/ros-perception/slam_gmapping.git
+
+# clone turtlebot teleop package
+# http://wiki.ros.org/turtlebot_teleop
+git clone https://github.com/turtlebot/turtlebot.git
+
+# clone turtlebot rviz launchers
+# http://wiki.ros.org/turtlebot_rviz_launchers
+#git clone https://github.com/turtlebot/turtlebot_interactions.git
+
+# clone turtlebot-gazebo
+# http://wiki.ros.org/turtlebot_gazebo
+git clone https://github.com/turtlebot/turtlebot_simulator.git
+
+# clone robot pose publisher
+# http://wiki.ros.org/robot_pose_publisher
+git clone https://github.com/GT-RAIL/robot_pose_publisher.git
+
+# clone teleop twist keyboard package
+# http://wiki.ros.org/teleop_twist_keyboard
+git clone https://github.com/ros-teleop/teleop_twist_keyboard.git
